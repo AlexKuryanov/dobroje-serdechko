3134055b7daa238f6d327aa5e6e91e7e9b0832c7
Elia <122890077+eleea556@users.noreply.github.com>
2023-06-27 22:29:35 +0200

    fix: fix main.js url detection

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-const currentPage = window.location.pathname;
+const currentPage = document.URL.split("/").pop();
 import "./mobile-menu.js";
 import "./scroll-up.js";
-if (currentPage === "/index.html") {
+if (currentPage === '' || currentPage === "index.html") {
   import("./slider.js");
   import("./swiper.js");
 }
