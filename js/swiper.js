f34e82525b80ebe60acd95327126a7324f839147
Elia <122890077+eleea556@users.noreply.github.com>
2023-06-30 15:19:10 +0200

    fix: swiper button behavior on Safari

--- js/swiper.js
+++ js/swiper.js
@@ -33,6 +33,34 @@ cards.forEach((card) => {
     });
 });
 
+if ('onscrollend' in window) {
+    prevBtn.disabled = true;
+    prevBtn.classList.add('btn-inactive');
+} else {
+    cardsSwiper.addEventListener('scroll', (e) => {
+        const maxScroll = cardsSwiper.scrollWidth - cardsSwiper.clientWidth;
+        if (cardsSwiper.scrollLeft != 0 && cardsSwiper.scrollLeft < maxScroll) {
+            prevBtn.classList.remove('btn-inactive');
+            nextBtn.classList.remove('btn-inactive');
+            prevBtn.disabled = false;
+            nextBtn.disabled = false;
+        }
+        if (cardsSwiper.scrollLeft === 0) {
+            prevBtn.classList.add('btn-inactive');
+            prevBtn.disabled = true;
+            nextBtn.disabled = false;
+            nextBtn.classList.remove('btn-inactive');
+        }
+        
+        if (cardsSwiper.scrollLeft >= maxScroll) {
+            nextBtn.classList.add('btn-inactive');
+            nextBtn.disabled = true;
+            prevBtn.disabled = false;
+            prevBtn.classList.remove('btn-inactive');
+        }
+    });
+}
+
 cardsSwiper.addEventListener('scrollend', (e) => {
     const maxScroll = cardsSwiper.scrollWidth - cardsSwiper.clientWidth;
     if (cardsSwiper.scrollLeft != 0 && cardsSwiper.scrollLeft < maxScroll) {
