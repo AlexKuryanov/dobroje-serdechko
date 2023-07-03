d9d593fb2d353c4d888e2d983c6fd277ab11f0fe
AlexKuryanov <lexxus07@yandex.ru>
2023-07-03 09:58:38 +0200

    refactor: formate and delete logs from slider&swiper

--- js/slider.js
+++ js/slider.js
@@ -1,39 +1,40 @@
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function checkHoverStatus(carousel) {
-    return carousel.matches(":focus") || carousel.matches(":hover") || scrolling;
+  return carousel.matches(":focus") || carousel.matches(":hover") || scrolling;
 }
 
 function scrollToNextSlide(carousel, element, isLast) {
-    if (isLast) {
-        carousel.scrollLeft = 0;
-    } else {
-        carousel.scrollLeft += element.offsetWidth;
-    }
+  if (isLast) {
+    carousel.scrollLeft = 0;
+  } else {
+    carousel.scrollLeft += element.offsetWidth;
+  }
 }
 
 function updateNavigation(navArray, radioArray) {
-    for (let i = 0; i < radioArray.length; i++) {
-        if (radioArray[i].checked) {
-          navArray[i].classList.add("checked");
-        } else {
-          navArray[i].classList.remove("checked");
-        }
+  for (let i = 0; i < radioArray.length; i++) {
+    if (radioArray[i].checked) {
+      navArray[i].classList.add("checked");
+    } else {
+      navArray[i].classList.remove("checked");
     }
+  }
 }
 
 function manuallyScroll(carousel, scrollLength) {
-    carousel.scrollLeft = scrollLength;
+  carousel.scrollLeft = scrollLength;
 }
 
 function updateScroll(carousel, slidesArray, slideWrap, navArray, radioArray) {
   const margin = parseInt(window.getComputedStyle(slideWrap[0], null).margin);
-  const i = Math.round(carousel.scrollLeft / (slidesArray[0].offsetWidth + 2*margin));
+  const i = Math.round(
+    carousel.scrollLeft / (slidesArray[0].offsetWidth + 2 * margin)
+  );
   radioArray[i].checked = true;
   updateNavigation(navArray, radioArray);
-  console.log(i);
 }
 
 function checkNavigationState(radioArray) {
@@ -45,22 +46,22 @@ function checkNavigationState(radioArray) {
 }
 
 function* loopSlides(carousel, list, navArray, radioArray) {
-    for (let i = 0; i < list.length; i++) {
-        i = checkNavigationState(radioArray);
-        const isLast = i === list.length -1 ? true : false;
-        scrollToNextSlide(carousel, list[i], isLast);
-        const nextButton = isLast ? radioArray[0] : radioArray[i+1];
-        nextButton.checked = true;
-        updateNavigation(navArray, radioArray);
-        if (isLast) {
-            i = -1;
-        }
-        yield i; 
+  for (let i = 0; i < list.length; i++) {
+    i = checkNavigationState(radioArray);
+    const isLast = i === list.length - 1 ? true : false;
+    scrollToNextSlide(carousel, list[i], isLast);
+    const nextButton = isLast ? radioArray[0] : radioArray[i + 1];
+    nextButton.checked = true;
+    updateNavigation(navArray, radioArray);
+    if (isLast) {
+      i = -1;
     }
+    yield i;
+  }
 }
 
 async function slider(viewport, slidesIterator) {
-  while(true) {
+  while (true) {
     await sleep(4000);
     const hovered = checkHoverStatus(viewport);
     if (!hovered) {
@@ -74,26 +75,32 @@ const navArray = document.getElementsByClassName("slider-nav-item");
 const radioArray = document.getElementsByClassName("slider-nav-button");
 const carousel = document.getElementById("slider");
 const slideWrap = document.getElementsByClassName("slide-wrap");
-const slidesIterator = loopSlides(carousel, slidesArray, navArray, radioArray, slideWrap);
+const slidesIterator = loopSlides(
+  carousel,
+  slidesArray,
+  navArray,
+  radioArray,
+  slideWrap
+);
 const viewport = document.getElementById("viewport-wrap");
 let scrolling = false;
 
 for (let i = 0; i < radioArray.length; i++) {
-    radioArray[i].addEventListener("click", (e) => {
-        const margin = parseInt(window.getComputedStyle(slideWrap[0], null).margin);
-        const scrollLength = i*(slidesArray[i].offsetWidth + 2*margin);
-        manuallyScroll(carousel, scrollLength);
-        updateNavigation(navArray, radioArray);
-    });
+  radioArray[i].addEventListener("click", (e) => {
+    const margin = parseInt(window.getComputedStyle(slideWrap[0], null).margin);
+    const scrollLength = i * (slidesArray[i].offsetWidth + 2 * margin);
+    manuallyScroll(carousel, scrollLength);
+    updateNavigation(navArray, radioArray);
+  });
 }
 
-carousel.addEventListener('scroll', (e) => {
+carousel.addEventListener("scroll", (e) => {
   scrolling = true;
 });
 
-carousel.addEventListener('scrollend', (e) => {
+carousel.addEventListener("scrollend", (e) => {
   updateScroll(carousel, slidesArray, slideWrap, navArray, radioArray);
-  scrolling = false
+  scrolling = false;
 });
 
 slider(viewport, slidesIterator);
--- js/swiper.js
+++ js/swiper.js
@@ -1,79 +1,79 @@
-let pos = {left: 0, x: 0};
+let pos = { left: 0, x: 0 };
 
 const cardsSwiper = document.querySelector(".cards-swiper");
 const cards = document.querySelectorAll(".card");
 const prevBtn = document.querySelector(".swiper-btn-prev");
-const nextBtn = document.querySelector(".swiper-btn-next")
+const nextBtn = document.querySelector(".swiper-btn-next");
 
 function mouseMoveHandler(e) {
-    const dx = e.clientX - pos.x;
+  const dx = e.clientX - pos.x;
 
-    cardsSwiper.scrollLeft = pos.left - dx;
+  cardsSwiper.scrollLeft = pos.left - dx;
 }
 
 function mouseUpHandler(e) {
-    cards.forEach((card) => {
-        card.classList.remove('grabbing');
-    });
-    document.removeEventListener('mousemove', mouseMoveHandler);
-    document.removeEventListener('mouseup', mouseUpHandler);
+  cards.forEach((card) => {
+    card.classList.remove("grabbing");
+  });
+  document.removeEventListener("mousemove", mouseMoveHandler);
+  document.removeEventListener("mouseup", mouseUpHandler);
 }
 
 cards.forEach((card) => {
-    card.addEventListener('mousedown', (e) => {
-        pos = {
-           left: cardsSwiper.scrollLeft,
-           x: e.clientX
-        };
-        cards.forEach((card) => {
-            card.classList.add('grabbing');
-        });
-        document.addEventListener('mousemove', mouseMoveHandler);
-        document.addEventListener('mouseup', mouseUpHandler);
+  card.addEventListener("mousedown", (e) => {
+    pos = {
+      left: cardsSwiper.scrollLeft,
+      x: e.clientX,
+    };
+    cards.forEach((card) => {
+      card.classList.add("grabbing");
     });
+    document.addEventListener("mousemove", mouseMoveHandler);
+    document.addEventListener("mouseup", mouseUpHandler);
+  });
 });
 
 function swiperButtonsHandler(e) {
-    const maxScroll = cardsSwiper.scrollWidth - cardsSwiper.clientWidth;
-    if (cardsSwiper.scrollLeft != 0 && cardsSwiper.scrollLeft < maxScroll) {
-        prevBtn.classList.remove('btn-inactive');
-        nextBtn.classList.remove('btn-inactive');
-        prevBtn.disabled = false;
-        nextBtn.disabled = false;
-    }
-    if (cardsSwiper.scrollLeft === 0) {
-        prevBtn.classList.add('btn-inactive');
-        prevBtn.disabled = true;
-        nextBtn.disabled = false;
-        nextBtn.classList.remove('btn-inactive');
-    }
-    
-    if (cardsSwiper.scrollLeft >= maxScroll) {
-        nextBtn.classList.add('btn-inactive');
-        nextBtn.disabled = true;
-        prevBtn.disabled = false;
-        prevBtn.classList.remove('btn-inactive');
-    }
+  const maxScroll = cardsSwiper.scrollWidth - cardsSwiper.clientWidth;
+  if (cardsSwiper.scrollLeft != 0 && cardsSwiper.scrollLeft < maxScroll) {
+    prevBtn.classList.remove("btn-inactive");
+    nextBtn.classList.remove("btn-inactive");
+    prevBtn.disabled = false;
+    nextBtn.disabled = false;
+  }
+  if (cardsSwiper.scrollLeft === 0) {
+    prevBtn.classList.add("btn-inactive");
+    prevBtn.disabled = true;
+    nextBtn.disabled = false;
+    nextBtn.classList.remove("btn-inactive");
+  }
+
+  if (cardsSwiper.scrollLeft >= maxScroll) {
+    nextBtn.classList.add("btn-inactive");
+    nextBtn.disabled = true;
+    prevBtn.disabled = false;
+    prevBtn.classList.remove("btn-inactive");
+  }
 }
 
-if ('onscrollend' in window) {
-    console.log('everything works fine');
-    cardsSwiper.addEventListener('scrollend', swiperButtonsHandler);
+if ("onscrollend" in window) {
+  cardsSwiper.addEventListener("scrollend", swiperButtonsHandler);
 } else {
-    cardsSwiper.addEventListener('scroll', swiperButtonsHandler);
+  cardsSwiper.addEventListener("scroll", swiperButtonsHandler);
 }
 
-
-prevBtn.addEventListener('click', (e) => {
-    cardsSwiper.classList.add('scroll-container');
-    const gap = parseInt(window.getComputedStyle(cardsSwiper, null).columnGap);
-    cardsSwiper.scrollLeft -= (parseInt(window.getComputedStyle(cards[0], null).width) + gap);
-    cardsSwiper.classList.remove('scroll-container');
+prevBtn.addEventListener("click", (e) => {
+  cardsSwiper.classList.add("scroll-container");
+  const gap = parseInt(window.getComputedStyle(cardsSwiper, null).columnGap);
+  cardsSwiper.scrollLeft -=
+    parseInt(window.getComputedStyle(cards[0], null).width) + gap;
+  cardsSwiper.classList.remove("scroll-container");
 });
 
-nextBtn.addEventListener('click', (e) => {
-    cardsSwiper.classList.add('scroll-container');
-    const gap = parseInt(window.getComputedStyle(cardsSwiper, null).columnGap);
-    cardsSwiper.scrollLeft += (parseInt(window.getComputedStyle(cards[0], null).width) + gap);
-    cardsSwiper.classList.remove('scroll-container');
+nextBtn.addEventListener("click", (e) => {
+  cardsSwiper.classList.add("scroll-container");
+  const gap = parseInt(window.getComputedStyle(cardsSwiper, null).columnGap);
+  cardsSwiper.scrollLeft +=
+    parseInt(window.getComputedStyle(cards[0], null).width) + gap;
+  cardsSwiper.classList.remove("scroll-container");
 });
